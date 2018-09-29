897be7966359d811463f6ce292e2319bc0b2a6ed
happyHooman <tornea.alexandru@gmail.com>
2018-09-30 01:28:25 +0300

    javascript

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-let map, cityBuildings;
+let map, cityBuildings, options;
 initWorld();
 displayWorldMap();
-addControls();
 
 
 function displayWorldMap() {
@@ -9,8 +8,13 @@ function displayWorldMap() {
     worldMap.classList.add('table', 'tabel-joc');
     for (let i = 0; i < map.length; i++) {
         let row = document.createElement('tr');
+        options = [
+            {name: '<i class="fas fa-sign-in-alt"></i>', cbk: 'enterCity()'},
+            {name: '<i class="fas fa-khanda"></i>', cbk: ''}
+        ];
+
         for (let j = 0; j < map[i].length; j++) {
-            row.innerHTML += map[i][j] === 0 ? '<td></td>\n' : '<td onclick="enterCity()"><i class="fab fa-fort-awesome"></i></td>\n';
+            row.innerHTML += map[i][j] === 0 ? '<td></td>\n' : `<td onclick="displayOptions(options)"><i class="fab fa-fort-awesome"></i></td>\n`;
         }
         worldMap.appendChild(row)
     }
@@ -26,6 +30,12 @@ function enterCity() {
         let row = document.createElement('tr');
         for (let j = 0; j < cityBuildings[i].length; j++) {
             let cell = document.createElement('td');
+
+            if ((i === 0 || i === 9) && j === 4) cell.style.borderRight = '4px solid darkblue';
+            if ((j === 0 || j === 9) && i === 4) cell.style.borderBottom = '4px solid darkblue';
+            if ((j === 1 || j === 9) && i < 9 && i > 0) cell.style.borderLeft = '4px solid darkblue';
+            if ((i === 1 || i === 9) && j < 9 && j > 0) cell.style.borderTop = '4px solid darkblue';
+
             switch (cityBuildings[i][j]) {
                 case 1:
                     cell.colSpan = 2;
@@ -39,10 +49,11 @@ function enterCity() {
         }
         city.appendChild(row);
     }
-    console.log(city);
     let container = document.getElementById('container');
     container.innerHTML = '';
     container.appendChild(city);
+    let options = [{name: '<i class="fas fa-globe"></i>', cbk: 'displayWorldMap()'}];
+    displayOptions(options);
 }
 
 function initWorld() {
@@ -73,7 +84,19 @@ function initWorld() {
     ];
 }
 
-function addControls() {
-    let keyboardControl = (e) => { if (e.keyCode === 87) displayWorldMap()};
-    window.addEventListener('keydown', keyboardControl);
+
+function displayOptions(options) {
+    let optionsTable = document.createElement('table');
+    let container = document.getElementById('container');
+
+    optionsTable.classList.add('table', 'city-table');
+
+    let row = document.createElement('tr');
+    for (let i = 0; i < options.length; i++) {
+        row.innerHTML += '<td onclick="' + options[i].cbk + '">' + options[i].name + '</td>\n';
+    }
+
+    optionsTable.appendChild(row);
+    if (container.childNodes.length>1) container.removeChild(container.childNodes[1]);
+    container.appendChild(optionsTable);
 }
